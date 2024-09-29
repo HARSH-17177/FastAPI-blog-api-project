a829e5eab828048103ab8d8128528691bf0e9cff
Harsh-17177 <harsh3101gupta@gmail.com>
2024-09-29 23:19:19 +0530

    first commit

---
+++ blog/schemas.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from typing import List,Optional
+
+
+# pydantic models are called schemas while 
+# sqlachamey models are called models
+class Blog(BaseModel):
+    title:str
+    body: str
+    
+class BlogBase(Blog):
+    class Config():
+        from_attributes=True    
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+
+# this is the model(schema) which we are using as response to show as alchamy model also return id
+class ShowUser(BaseModel):
+    name:str
+    email:str  
+    blogs: List[BlogBase]=[]
+    class Config():
+        from_attributes=True   
+
+
+class ShowBlog(BaseModel):
+    title:str
+    body: str    
+    creator: ShowUser
+    class Config():
+        from_attributes=True   
+
+
+class Login(BaseModel):
+    username:str
+    password:str          
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None           
